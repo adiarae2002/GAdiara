b1c1585bee11184a223fc6a8e79d436ee431a8fc
whois <whois@192.168.1.136>
2026-05-03 08:42:46 +0200

    Modificación en la ramadiara

--- pAdiara/src/paqAdiara/ClaseAdiara.java
+++ pAdiara/src/paqAdiara/ClaseAdiara.java
@@ -4,5 +4,6 @@ public class ClaseAdiara {
 
     public static void main(String[] args) {
         System.out.println("Esta es la clase nueva en la rama ramadiara");
+        System.out.println("Modificación en la rama ramadiara");
     }
 }
